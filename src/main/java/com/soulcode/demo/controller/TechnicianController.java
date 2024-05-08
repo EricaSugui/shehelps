b1cc75ab6c62b196fa0f44a8a90679c04db8bb07
DoriansFer <doohfernandes@yahoo.com>
2024-05-08 20:06:43 -0300

    technician

---
+++ src/main/java/com/soulcode/demo/controller/TechnicianController.java
@@ -0,0 +1,32 @@
+package com.soulcode.demo.controller;
+
+import com.soulcode.demo.models.Ticket;
+import com.soulcode.demo.repositories.TicketRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+public class TechnicianController {
+    TicketRepository ticketRepository;
+
+    public TechnicianController(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+
+
+
+    @GetMapping("/")
+    public String telaTecnico(Model model) {
+
+        List<Ticket> tickets = ticketRepository.findByStatus("Aguardando atendimento");
+
+        model.addAttribute("tickets", tickets);
+
+        System.out.println("tickets:" + tickets);
+
+        return "/";
+
+    }
+
+}
--- src/main/java/com/soulcode/demo/repositories/TicketRepository.java
+++ src/main/java/com/soulcode/demo/repositories/TicketRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
-
+        List<Ticket>findByStatus(String status);
 
 }
