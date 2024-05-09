ecc6a1a31c7039e4e9e7b958fc6fe64b53a410f3
DoriansFer <doohfernandes@yahoo.com>
2024-05-09 20:12:18 -0300

    technician ticket

--- src/main/java/com/soulcode/demo/repositories/TicketRepository.java
+++ src/main/java/com/soulcode/demo/repositories/TicketRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
+    List<Ticket>findByPrioridade(String prioridade);
 
 
 }
