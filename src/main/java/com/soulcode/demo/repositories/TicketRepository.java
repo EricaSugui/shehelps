5c6d32b9837a9761848ae6a0911e8917faa7771e
danibotelho <danibl26@gmail.com>
2024-05-12 11:57:23 -0300

    criacao da tela edit-ticket e logica para o admin editar o chamado

--- src/main/java/com/soulcode/demo/repositories/TicketRepository.java
+++ src/main/java/com/soulcode/demo/repositories/TicketRepository.java
@@ -3,14 +3,18 @@ package com.soulcode.demo.repositories;
 import com.soulcode.demo.models.Sector;
 import com.soulcode.demo.models.Status;
 import com.soulcode.demo.models.Ticket;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
     List<Ticket>findByPrioridade(String prioridade);
     List<Ticket> findBySetorDeDirecionamento(Sector sector);
     List<Ticket> findByStatus(Status status);
-
+    Optional<Ticket> findById(Long id);
 }
